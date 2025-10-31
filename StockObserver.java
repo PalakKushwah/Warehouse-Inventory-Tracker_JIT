c250f6da176c7ca5223b6430cd9272af10bde44e
Palak kushwah <palakkush0@gmail.com>
2025-10-31 14:47:45 +0530

    Update StockObserver.java

--- StockObserver.java
+++ StockObserver.java
@@ -3,7 +3,5 @@ package com.inventory.observer;
 import com.inventory.model.Product;
 
 public interface StockObserver {
-   
-    void onStockChanged(Product product);
+    void onLowStock(Product product);
 }
-
